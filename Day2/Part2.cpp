ac6ccb890fb7c33f18b88184f792b7f6bc321d7d
SamBobBarnes <samuelrbarnes@outlook.com>
2024-11-11 08:55:21 -0600

    wip day 2 part 2

--- Day2/Part2.cpp
+++ Day2/Part2.cpp
@@ -2,34 +2,48 @@
 // Created by Samuel Barnes on 11/11/24.
 //
 
-#include <fstream>
+
 #include <iostream>
 #include <vector>
+#include "../Helpers.h"
 
 #include "Day2.h"
 
 int Day2::Part2() {
-    std::vector<std::string> words{
+    const std::string filePath{"Day2/input.txt"};
+    std::vector<std::string> initialWords{
         "RLZ", "HRHZ", "G", "CT", "DCC", "OW", "LXLQ", "QXS", "ENS", "MGUU", "VUL", "U", "IQSZTWLQLJ", "SRJITALTAM",
         "DR", "CC", "NBLR", "WXVAPEAUJM", "TGSSQZPJNI", "E", "Y", "YEG", "CPJ", "MJ", "XW", "PITF", "UYUCEJBSRS",
         "DSSG", "WOW", "OO", "WP", "UOB", "GT", "A", "DPDVINCYYQ", "JW", "SXLE", "KJWV", "PKNW", "ZMNJHSVPWW", "RBY",
         "IFQF", "AXHIDHBYEW", "SM", "ECWJZXYPLY", "EOB", "QF", "VAGCFZRJUK"
     };
-
-    for (const auto &word: words) {
-        std::string reverse{""};
-        for (int i = word.length(); i >= 0; i--) {
-            reverse.push_back(word[i]);
+    std::vector<std::string> words{};
+    for (const auto &word: initialWords) {
+        words.push_back(word);
+        if (word.length() > 1) {
+            std::string reverse{""};
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reverse.push_back(word[i]);
+            }
+            words.push_back(reverse);
         }
-        words.push_back(reverse);
     }
-    std::vector<std::vector<std::string> > tokenList{};
-
-    std::cout << system("pwd") << std::endl;
-    std::ifstream inputFile("../Day2/input.txt");
-    std::string line;
-    long count = inputFile.gcount();
+    std::vector<std::string> lines = Helpers::readFile(filePath);
 
 
     int total = 0;
+
+    for (const auto &line: lines) {
+        std::vector<std::string> tokens = split(line);
+        for (const auto &token: tokens) {
+            for (const auto &word: words) {
+                if (token.find(word) != -1) {
+                    std::cout << token << " " << word << std::endl;
+                    total++;
+                }
+            }
+        }
+    }
+
+    return total;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+
+#include "Helpers.h"
 #include "Day1/Day1.h"
 #include "Day2/Day2.h"
 
 int main() {
-    const int day = 2;
-    switch (day) {
+    std::cout << "Current dir: " << Helpers::pwd() << std::endl;
+    switch (2) {
+        case 1:
+            std::cout << "Day 1 - Part 1: " << Day1::Part1() << std::endl;
+            std::cout << "Day 1 - Part 2: " << Day1::Part2() << std::endl;
+            std::cout << "Day 1 - Part 3: " << Day1::Part3() << std::endl;
         case 2:
             std::cout << "Day 2 - Part 1: " << Day2::Part1() << std::endl;
             std::cout << "Day 2 - Part 2: " << Day2::Part2() << std::endl;

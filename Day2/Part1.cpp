038f5f6f893c3bb24075ad9a12fdaf0f763b3413
SamBobBarnes <samuelrbarnes@outlook.com>
2024-11-11 00:35:17 -0600

    day 2 part 1

--- Day2/Part1.cpp
+++ Day2/Part1.cpp
@@ -2,16 +2,30 @@
 // Created by Samuel Barnes on 11/10/24.
 //
 
+#include <iostream>
 #include <string>
 #include <vector>
 
 #include "Day2.h"
 
 int Day2::Part1() {
-    const std::string words[]{"LOR", "LL", "SI", "OR", "EN", "ON", "UM"};
+    const std::vector<std::string> words{"LOR", "LL", "SI", "OR", "EN", "ON", "UM"};
     const std::string input{
         "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR. EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM."
     };
 
-    std::vector<std::string> tokens{};
+    std::vector<std::string> tokens = split(input);
+
+    int total = 0;
+
+    for (const auto &token: tokens) {
+        for (const auto &word: words) {
+            if (token.find(word) != -1) {
+                std::cout << token << " " << word << std::endl;
+                total++;
+            }
+        }
+    }
+
+    return total;
 }
--- main.cpp
+++ main.cpp
@@ -5,11 +5,6 @@
 int main() {
     const int day = 2;
     switch (day) {
-        case 1:
-            std::cout << "Day 1 - Part 1: " << Day1::Part1() << std::endl;
-            std::cout << "Day 1 - Part 2: " << Day1::Part2() << std::endl;
-            std::cout << "Day 1 - Part 3: " << Day1::Part3() << std::endl;
-            break;
         case 2:
             std::cout << "Day 2 - Part 1: " << Day2::Part1() << std::endl;
         // std::cout << "Day 2 - Part 2: " << Day2::Part2() << std::endl;

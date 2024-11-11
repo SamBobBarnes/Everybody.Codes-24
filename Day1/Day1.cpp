67c9906a4543b9e5acc2b56984b4c610e613b474
SamBobBarnes <samuelrbarnes@outlook.com>
2024-11-10 22:27:56 -0600

    day 1 init

---
+++ Day1/Day1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Samuel Barnes on 11/10/24.
+//
+
+#include "Day1.h"
---
+++ Day1/Day1.h
@@ -0,0 +1,16 @@
+//
+// Created by Samuel Barnes on 11/10/24.
+//
+
+#ifndef DAY1_H
+#define DAY1_H
+
+
+
+class Day1 {
+
+};
+
+
+
+#endif //DAY1_H

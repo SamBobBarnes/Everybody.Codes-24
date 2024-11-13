d424dffaa208295ea0308baf3b05bd302d7aa031
sb17057 <sbarnes@vermeer.com>
2024-11-13 16:52:18 -0600

    wip part3

--- Day4/Day4.h
+++ Day4/Day4.h
@@ -15,5 +15,14 @@ public:
     static int Part2();
 
     static int Part3();
+
+private:
+    static std::string leftpad(const std::string &str, int width, char fill = ' ') {
+        if (str.length() >= width) {
+            return str;
+        }
+
+        return std::string(width - str.length(), fill) + str;
+    }
 };
 #endif //DAY4_H
--- Day4/Part3.cpp
+++ Day4/Part3.cpp
@@ -3,7 +3,48 @@
 //
 
 #include "Day4.h"
+#include <string>
 
 int Day4::Part3() {
-    return 0;
+    auto lines = Helpers::readFile("Day4/Part3.txt");
+    vector<int> nails{};
+
+    int runningTotal{0};
+    int max{0};
+    int min{0};
+
+    for (const auto &line: lines) {
+        int nail = std::stoi(line) - 27000000;
+        if (max == 0 || nail > max) max = nail;
+        if (min == 0 || nail < min) min = nail;
+        runningTotal += nail;
+        nails.push_back(nail);
+    }
+    vector<tuple<int, int, int, int> > totals{};
+    int goal = runningTotal / lines.size() + 1;
+    int ups{0};
+    int downs{0};
+
+    int total{0};
+    for (int nail: nails) {
+        if (nail < goal) {
+            total += goal - nail;
+            ups += goal - nail;
+        } else {
+            total += nail - goal;
+            downs += nail - goal;
+        }
+    }
+
+    totals.emplace_back(goal, ups, downs, total);
+
+    cout << endl;
+    cout << leftpad("Average", 10) << " |" << leftpad("Ups", 10) << " |" << leftpad("Downs", 10) << " |" <<
+            leftpad("Total", 10) << endl;
+    for (auto &line: totals) {
+        cout << leftpad(to_string(get<0>(line)), 10) << " |" << leftpad(to_string(get<1>(line)), 10) << " |" <<
+                leftpad(to_string(get<2>(line)), 10) << " |" << leftpad(to_string(get<3>(line)), 10) << endl;
+    }
+
+    return total;
 }

12afd86357a712507e8daad669a468da4a6763b2
sb17057 <sbarnes@vermeer.com>
2024-11-13 10:13:24 -0600

    tests

--- tests/Day2.cpp
+++ tests/Day2.cpp
@@ -1,5 +1,3 @@
-#include <map>
-
 #include "../Day2/FindResult.h"
 #include "../Day2/Split.cpp"
 #include "../Day2/findWordOnGrid.cpp"
@@ -19,8 +17,9 @@ bool operator==(const Point &lhs, const Point &rhs) {
 #pragma endregion Comparators
 
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_all.hpp>
-#include <catch2/generators/catch_generators_all.hpp>
+#include <catch2/matchers/catch_matchers_range_equals.hpp>
+#include <catch2/matchers/catch_matchers_container_properties.hpp>
+#include <catch2/generators/catch_generators_adapters.hpp>
 
 #pragma region Split
 TEST_CASE("Split converts string into list of strings", "[split]") {
@@ -62,6 +61,63 @@ TEST_CASE("FindWordOnGrid does not find word", "[findWordOnGrid]") {
     REQUIRE_THAT(actual.symbolLocations, Catch::Matchers::IsEmpty());
 }
 
+TEST_CASE("FindWordOnGrid finds single letter words", "[findWordOnGrid]") {
+    char grid[][6]{
+        {'F', 'F', 'E', 'S', 'D', 'R'},
+        {'F', 'F', 'K', 'S', 'D', 'R'},
+        {'F', 'F', 'Q', 'S', 'D', 'R'},
+        {'F', 'I', 'E', 'I', 'D', 'R'},
+        {'F', 'F', 'E', 'D', 'D', 'R'}
+    };
+    auto [wordToFind, branch] = GENERATE(table<std::string, int>({
+                                             std::make_tuple("F", 0),
+                                             std::make_tuple("K", 1),
+                                             std::make_tuple("S", 2),
+                                             std::make_tuple("E", 3),
+                                             std::make_tuple("D", 4),
+                                             std::make_tuple("R", 5),
+                                             std::make_tuple("I", 6),
+                                             std::make_tuple("Q", 7)
+                                             }));
+    SECTION(wordToFind) {
+        std::vector<Point> expected;
+        switch (branch) {
+            case 0:
+                expected = {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}, {0, 3}, {0, 4}, {1, 4}};
+                break;
+            case 1:
+                expected = {{2, 1}};
+                break;
+            case 2:
+                expected = {{3, 0}, {3, 1}, {3, 2}};
+                break;
+            case 3:
+                expected = {{2, 0}, {2, 3}, {2, 4}};
+                break;
+            case 4:
+                expected = {{4, 0}, {4, 1}, {4, 2}, {4, 3}, {3, 4}, {4, 4}};
+                break;
+            case 5:
+                expected = {{5, 0}, {5, 1}, {5, 2}, {5, 3}, {5, 4}};
+                break;
+            case 6:
+                expected = {{1, 3}, {3, 3}};
+                break;
+            case 7:
+                expected = {{2, 2}};
+                break;
+            default:
+                std::cout << "TILT" << std::endl;
+                Catch::throw_test_failure_exception();
+        }
+
+        FindResult actual = Day2::findWordOnGrid(6, 5, reinterpret_cast<char *>(grid), wordToFind);
+
+        REQUIRE(actual.found == true);
+        REQUIRE_THAT(actual.symbolLocations, Catch::Matchers::UnorderedRangeEquals(expected));
+    }
+}
+
 TEST_CASE("FindWordOnGrid finds single word", "[findWordOnGrid]") {
     char grid[][6]{
         {'F', 'F', 'E', 'S', 'D', 'R'},
@@ -70,18 +126,15 @@ TEST_CASE("FindWordOnGrid finds single word", "[findWordOnGrid]") {
         {'F', 'I', 'E', 'I', 'D', 'R'},
         {'F', 'F', 'E', 'D', 'D', 'R'}
     };
-    std::string testName;
-    std::string wordToFind;
-    int direction;
-    std::tie(testName, wordToFind, direction) = GENERATE(table<std::string, std::string, int>(
-                                                             {
-                                                             std::make_tuple("Left to Right","FES",0),
-                                                             std::make_tuple("Right to Left","SEF",0),
-                                                             std::make_tuple("Down","SID",1),
-                                                             std::make_tuple("Up","DIS",1),
-                                                             std::make_tuple("Left Wrap","DRFI",2),
-                                                             std::make_tuple("Right Wrap","IFRD",2)
-                                                             }));
+    auto [testName, wordToFind, direction] = GENERATE(table<std::string, std::string, int>(
+                                                          {
+                                                          std::make_tuple("Left to Right","FES",0),
+                                                          std::make_tuple("Right to Left","SEF",0),
+                                                          std::make_tuple("Down","SID",1),
+                                                          std::make_tuple("Up","DIS",1),
+                                                          std::make_tuple("Left Wrap","DRFI",2),
+                                                          std::make_tuple("Right Wrap","IFRD",2)
+                                                          }));
     SECTION(testName) {
         std::vector<Point> expected;
         switch (direction) {

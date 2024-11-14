8679bf244ceae7e557c3a02c6f440096c03c5705
sb17057 <sbarnes@vermeer.com>
2024-11-14 08:44:13 -0600

    day 4 part 3 finished! brute force worked

--- Day4/Part3.cpp
+++ Day4/Part3.cpp
@@ -2,6 +2,8 @@
 // Created by sb17057 on 11/13/2024.
 //
 
+#include <algorithm>
+
 #include "Day4.h"
 #include <string>
 
@@ -21,6 +23,7 @@ int Day4::Part3() {
         nails.push_back(nail);
     }
     vector<tuple<int, int, int, int> > totals{};
+
     int goal = runningTotal / lines.size() + 1;
     int ups{0};
     int downs{0};
@@ -38,12 +41,50 @@ int Day4::Part3() {
 
     totals.emplace_back(goal, ups, downs, total);
 
+    ranges::sort(nails);
+
+    for (int goalNail: nails) {
+        ups = 0;
+        downs = 0;
+        total = 0;
+
+        for (int nail: nails) {
+            if (nail < goalNail) {
+                total += goalNail - nail;
+                ups += goalNail - nail;
+            } else {
+                total += nail - goalNail;
+                downs += nail - goalNail;
+            }
+        }
+        totals.emplace_back(goalNail, ups, downs, total);
+    }
+
+    int lowestTotal{0};
+    int lowestTotalIndex{0};
+    int index{0};
+    for (auto &i: totals) {
+        int temp = get<3>(i);
+        if (lowestTotal == 0 || lowestTotal > temp) {
+            lowestTotal = temp;
+            lowestTotalIndex = index;
+        }
+        index++;
+    }
+
+    index = 0;
+    const char alert[] = "\033[31;4m";
+    const char reset[] = "\033[0m";
+
     cout << endl;
     cout << leftpad("Average", 10) << " |" << leftpad("Ups", 10) << " |" << leftpad("Downs", 10) << " |" <<
             leftpad("Total", 10) << endl;
     for (auto &line: totals) {
+        if (lowestTotalIndex == index) cout << alert;
+        else cout << reset;
         cout << leftpad(to_string(get<0>(line)), 10) << " |" << leftpad(to_string(get<1>(line)), 10) << " |" <<
                 leftpad(to_string(get<2>(line)), 10) << " |" << leftpad(to_string(get<3>(line)), 10) << endl;
+        index++;
     }
 
     return total;

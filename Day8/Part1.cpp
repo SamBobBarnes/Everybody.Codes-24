dcca3d3468bc86a31eb1b61450f230ee268c914f
sb17057 <sbarnes@vermeer.com>
2024-11-18 11:35:53 -0600

    day 8 part 1

--- Day8/Part1.cpp
+++ Day8/Part1.cpp
@@ -1,7 +1,29 @@
 #include "Day8.h"
 
-int Day8::Part1() {
-    auto line = Helpers::readFile(8, 1);
 
-    return 0;
+int WidthOfTower(int height) {
+    return 1 + 2 * (height - 1);
+}
+
+int TotalBlocks(int height) {
+    int total{0};
+    for (int i = 1; i <= height; i++) {
+        total += WidthOfTower(i);
+    }
+    return total;
+}
+
+int Day8::Part1() {
+    int blocksAvailable = stoi(Helpers::readFile(8, 1)[0]);
+    int index{0};
+    int currentUsage{0};
+    while (currentUsage <= blocksAvailable) {
+        index++;
+        currentUsage = TotalBlocks(index);
+    }
+
+    int width = WidthOfTower(index);
+    int blocksNeeded = currentUsage - blocksAvailable;
+
+    return width * blocksNeeded;
 }

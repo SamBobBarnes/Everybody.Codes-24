4cf52b5aa4ea6cba299d805a985eca2da3604c86
sb17057 <sbarnes@vermeer.com>
2024-11-21 15:06:44 -0600

    day 11 part 1

--- Day11/Part1.cpp
+++ Day11/Part1.cpp
@@ -1,7 +1,34 @@
+#include <map>
+
 #include "Day11.h"
 
 int Day11::Part1() {
     auto lines = Helpers::readFile(11, 1);
+    map<char, vector<char> > conversions{};
+    vector<char> population{'A'};
 
-    return 0;
+    for (auto &line: lines) {
+        auto parts = Helpers::split(line, ':');
+        auto parent = parts[0];
+        auto offspring = Helpers::split(parts[1], ',');
+
+        vector<char> offspringChars{};
+        for (auto child: offspring) {
+            offspringChars.push_back(child[0]);
+        }
+        conversions.insert({parent[0], offspringChars});
+    }
+
+    int rounds{4};
+
+    for (int i: views::iota(0, rounds)) {
+        vector<char> nextGen{};
+
+        for (char bug: population) {
+            nextGen.append_range(conversions[bug]);
+        }
+        population = move(nextGen);
+    }
+
+    return population.size();
 }

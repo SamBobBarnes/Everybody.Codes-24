334b476c601853d21c8426eefe3957c88965c47a
sb17057 <sbarnes@vermeer.com>
2024-11-21 22:20:03 -0600

    day 12 wip

--- Day12/Part1.cpp
+++ Day12/Part1.cpp
@@ -1,4 +1,9 @@
 #include "Day12.h"
+#include "../Point.h"
+
+int GetLandingDistance(int height, int power) {
+    return power * 3 + height - 1;
+}
 
 int Day12::Part1() {
     auto lines = Helpers::readFile(12, 1);

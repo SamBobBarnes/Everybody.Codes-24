d8e6929215f903beca6d781d1f94c7ce6cce451a
sb17057 <sbarnes@vermeer.com>
2024-11-24 07:39:57 -0600

    day 14 part 2

--- Day14/Part2.cpp
+++ Day14/Part2.cpp
@@ -6,16 +6,54 @@ int Day14::Part2() {
     auto lines = Helpers::readFile(14, 2);
     vector<vector<GrowthPlan> > plans{};
 
-    set<Point> uniquePoints{};
+    set<Point3> uniquePoints{};
 
     for (auto line: lines) {
         Point3 current{0, 0, 0};
-        auto directions = Helpers::split(lines[0], ',');
+        auto directions = Helpers::split(line, ',');
         for (auto direction: directions) {
             GrowthPlan plan{direction};
+            switch (plan.direction) {
+                case Up:
+                    for (int i = 0; i < plan.distance; ++i) {
+                        current.y++;
+                        uniquePoints.insert(current);
+                    }
+                    break;
+                case Down:
+                    for (int i = 0; i < plan.distance; ++i) {
+                        current.y--;
+                        uniquePoints.insert(current);
+                    }
+                    break;
+                case Left:
+                    for (int i = 0; i < plan.distance; ++i) {
+                        current.x--;
+                        uniquePoints.insert(current);
+                    }
+                    break;
+                case Right:
+                    for (int i = 0; i < plan.distance; ++i) {
+                        current.x++;
+                        uniquePoints.insert(current);
+                    }
+                    break;
+                case Forward:
+                    for (int i = 0; i < plan.distance; ++i) {
+                        current.z++;
+                        uniquePoints.insert(current);
+                    }
+                    break;
+                case Backward:
+                    for (int i = 0; i < plan.distance; ++i) {
+                        current.z--;
+                        uniquePoints.insert(current);
+                    }
+                    break;
+            }
         }
     }
 
 
-    return 0;
+    return uniquePoints.size();
 }
